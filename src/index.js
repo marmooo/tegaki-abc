91ebb0d08f3aab506d2bce638822c33a5b23e851
marmooo <marmooo@users.noreply.github.com>
2022-06-08 00:37:03 +0900

    add yomico

--- src/index.js
+++ src/index.js
@@ -30,6 +30,25 @@ function toggleDarkMode() {
     localStorage.setItem("darkMode", 1);
     document.documentElement.dataset.theme = "dark";
   }
+  if (localStorage.getItem("furigana") == 1) {
+    const obj = document.getElementById("addFurigana");
+    addFurigana(obj);
+    obj.setAttribute("data-done", true);
+  }
+}
+
+function addFurigana() {
+  const obj = document.getElementById("addFurigana");
+  if (obj.getAttribute("data-done")) {
+    localStorage.setItem("furigana", 0);
+    location.reload();
+  } else {
+    import("https://marmooo.github.io/yomico/yomico.min.js").then((module) => {
+      module.yomico("index.yomi");
+    });
+    localStorage.setItem("furigana", 1);
+    obj.setAttribute("data-done", true);
+  }
 }
 
 function playAudio(audioBuffer, volume) {
@@ -337,6 +356,7 @@ worker.addEventListener("message", (e) => {
 });
 
 document.getElementById("toggleDarkMode").onclick = toggleDarkMode;
+document.getElementById("addFurigana").onclick = addFurigana;
 document.getElementById("restartButton").onclick = countdown;
 document.getElementById("startButton").onclick = countdown;
 document.getElementById("showAnswer").onclick = showAnswer;
--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,8 @@
-var CACHE_NAME = "2022-06-08 00:10";
+var CACHE_NAME = "2022-06-08 00:25";
 var urlsToCache = [
   "/tegaki-abc/",
   "/tegaki-abc/index.js",
+  "/tegaki-abc/index.yomi",
   "/tegaki-abc/worker.js",
   "/tegaki-abc/model/model.json",
   "/tegaki-abc/model/group1-shard1of1.bin",
@@ -11,6 +12,7 @@ var urlsToCache = [
   "/tegaki-abc/mp3/correct3.mp3",
   "/tegaki-abc/kohacu.webp",
   "/tegaki-abc/favicon/favicon.svg",
+  "https://marmooo.github.io/yomico/yomico.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.17.0/dist/tf.min.js",
