a5739951a0f8b128ec0762434c17eb60438cb468
marmooo <marmooo@users.noreply.github.com>
2022-06-11 11:07:15 +0900

    bump signature_pad from 4.0.4 to 4.0.5

--- src/sw.js
+++ src/sw.js
@@ -16,7 +16,7 @@ var urlsToCache = [
   "/tegaki-abc/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.5/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.17.0/dist/tf.min.js",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
