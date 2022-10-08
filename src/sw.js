89c6fe364173cc56963b5d6daf8eb81357da2e0a
marmooo <marmooo@users.noreply.github.com>
2022-10-08 11:14:51 +0900

    bump bootstrap from 5.2.1 to 5.2.2

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-09-25 10:38";
+var CACHE_NAME = "2022-10-08 11:03";
 var urlsToCache = [
   "/tegaki-abc/",
   "/tegaki-abc/index.js",
@@ -15,7 +15,7 @@ var urlsToCache = [
   "/tegaki-abc/refresh.svg",
   "/tegaki-abc/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.9/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.20.0/dist/tf.min.js",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
