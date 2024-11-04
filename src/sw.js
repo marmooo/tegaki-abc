1b164d2982f5206e4263f91e1e2ed940cf46b5f3
marmooo <marmooo@users.noreply.github.com>
2024-11-04 13:29:38 +0900

    bump tfjs from 4.21.0 to 4.22.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-10-30 00:43";
+const CACHE_NAME = "2024-11-04 13:29";
 const urlsToCache = [
   "/tegaki-abc/",
   "/tegaki-abc/index.js",
@@ -12,7 +12,7 @@ const urlsToCache = [
   "/tegaki-abc/kohacu.webp",
   "/tegaki-abc/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.21.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
 
